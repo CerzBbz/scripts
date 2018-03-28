05fad9f7741c54669214eb7c16f66a0ad710abcd
Cerz <robin_thompson@live.co.uk>
2018-03-28 21:39:11 +0100

    Updating display script to support different configs

--- display.sh
+++ display.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-xrandr --output VGA-1 --off --output eDP-1 --primary --auto --output HDMI-1 --auto --right-of eDP-1
+if [ "$1" = "laptop" ]; then
+    xrandr --output VGA-1 --off --output eDP-1 --primary --auto --output HDMI-1 --off
+elif [ "$1" = "tv" ]; then
+    xrandr --output VGA-1 --off --output eDP-1 --off --output HDMI-1 --auto
+else
+    xrandr --output VGA-1 --off --output eDP-1 --primary --auto --output HDMI-1 --auto --right-of eDP-1
+fi
