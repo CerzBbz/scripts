9197befff8628ee6a887d2c1fab36c5b06900318
Robin <robin_thompson@live.co.uk>
2023-05-14 20:59:15 +0800

    chore: don't output null-null

--- currently-playing.sh
+++ currently-playing.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-url="http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${LAST_FM_USER}&api_key=${LAST_FM_KEY}&format=json"
-curl $url 2>/dev/null | jq '.recenttracks.track[0].artist["#text"], .recenttracks.track[0].name' -r | sed ':a;N;$!ba;s/\n/ - /g'
+url="http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${LAST_FM_USER}&api_key=${LAST_FM_KEY}&format=json&limit=1"
+song=`curl $url 2>/dev/null | jq '.recenttracks.track[0] | .artist["#text"], .name // empty' -r | sed ':a;N;$!ba;s/\n/ - /g'`
+if [[ $song != null ]]; then
+  echo $song
+fi
