fa9c7e59e600c423082334449784020a72c57fee
robin <robin_thompson@live.co.uk>
2025-07-21 18:00:32 +0100

    🐛 fix for songs or artists named *

--- currently-playing.sh
+++ currently-playing.sh
@@ -3,5 +3,5 @@
 url="http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${LAST_FM_USER}&api_key=${LAST_FM_KEY}&format=json&limit=1"
 song=`curl $url 2>/dev/null | jq '.recenttracks.track[0] | .artist["#text"], .name // empty' -r | sed ':a;N;$!ba;s/\n/ - /g'`
 if [[ $song != null ]]; then
-  echo $song
+  echo "$song"
 fi
